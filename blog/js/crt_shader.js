95a9c5e219886f2312ca8defc2fceb2226b0ef03
Turtle <turtle@laserturtle.net>
2022-01-25 13:42:36 -0800

    Update blog theme

---
+++ blog/js/crt_shader.js
@@ -0,0 +1,128 @@
+'use strict';
+
+let gl = null;
+let canvas = null;
+let time_uniform = null;
+let sampler_uniform = null;
+
+function loadTexture(gl, url)
+{
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    const pixel = new Uint8Array([0, 0, 255, 255]);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, pixel);
+
+    const image = new Image();
+    image.onload = function() {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    }
+
+    image.src = url;
+
+    return texture;
+}
+
+function compileShader(id, type)
+{
+    let code = document.getElementById(id).firstChild.nodeValue;
+    let shader = gl.createShader(type);
+
+    gl.shaderSource(shader, code);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+    {
+        console.log("Error compiling", id);
+        console.log(gl.getShaderInfoLog(shader));
+    }
+
+    return shader;
+}
+
+function makeShader(info)
+{
+    let program = gl.createProgram();
+    info.forEach(function(desc) {
+        let shader = compileShader(desc.id, desc.type);
+        if (shader) {
+            gl.attachShader(program, shader);
+        }
+    });
+
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS))
+    {
+        console.log("Error linking");
+        console.log(gl.getProgramInfoLog(program));
+    }
+
+    return program;
+}
+
+function setup()
+{
+    canvas = document.getElementById('glcanvas');
+    gl = glcanvas.getContext('webgl');
+
+    if (!gl)
+    {
+        const img = document.getElementById("backup-image");
+        img.style.display = 'initial';
+        return;
+    }
+
+    const shaders = [
+        { type: gl.VERTEX_SHADER, id: 'vertex-shader' },
+        { type: gl.FRAGMENT_SHADER, id: 'fragment-shader' }
+    ];
+
+    let shader = makeShader(shaders);
+
+    const vertex_array = new Float32Array([
+        -1.0,  1.0, 
+         1.0,  1.0, 
+         1.0, -1.0,
+
+        -1.0,  1.0, 
+         1.0, -1.0, 
+        -1.0, -1.0
+    ]);
+
+    gl.useProgram(shader);
+    let vertices = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertices);
+    let attrib = gl.getAttribLocation(shader, 'position');
+    gl.enableVertexAttribArray(attrib);
+    gl.vertexAttribPointer(attrib, 2, gl.FLOAT, false, 0, 0);
+    gl.bufferData(gl.ARRAY_BUFFER, vertex_array, gl.STATIC_DRAW);
+
+    time_uniform = gl.getUniformLocation(shader, 'uTime');
+    sampler_uniform = gl.getUniformLocation(shader, 'uSampler');
+
+    let texture = loadTexture(gl, '/blog/images/ocaml.png');
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.uniform1i(sampler_uniform, 0);
+
+    gl.clearColor(0, 0, 0, 1);
+
+    window.requestAnimationFrame(animate);
+}
+
+function animate(timestamp)
+{
+    gl.viewport(0, 0, glcanvas.width, glcanvas.height);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.uniform1f(time_uniform, timestamp/1000);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+    
+    window.requestAnimationFrame(animate); 
+}
