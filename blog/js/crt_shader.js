2a1e8d1f9f20011bad9a76c5bf5bff69d82cf2e4
Turtle <turtle@laserturtle.net>
2022-01-30 12:30:03 -0800

    Change the default color from blue to white

--- blog/js/crt_shader.js
+++ blog/js/crt_shader.js
@@ -10,7 +10,7 @@ function loadTexture(gl, url)
     const texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
 
-    const pixel = new Uint8Array([0, 0, 255, 255]);
+    const pixel = new Uint8Array([255, 255, 255, 255]);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, pixel);
 
     const image = new Image();
